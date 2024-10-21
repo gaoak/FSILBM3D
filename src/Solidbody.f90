a0dc8dfe960e5aa707ca059405e397aef84dd22e
astrobearrrrr <895208821@qq.com>
2024-10-21 16:18:32 +0800

    add update section, xyz and rotation

--- src/Solidbody.f90
+++ src/Solidbody.f90
@@ -110,7 +110,31 @@ module SolidBody
         real(8), intent(in):: updaterealxyzful(real_npts,6)
         integer :: i,j
         real(8) :: dxyz0(3),dxyz(3),xlmn0(3),xlmn(3),dl0,dl,temp(3),tempdxyz(3)
-        
+        do j = 1,Beam%fake_npts
+            do i = 1,real_npts
+                if (Beam%fake_sec(i,j) .ne. 0) then
+                    if ( i .eq. 1) then
+                        dxyz0(1:3)= real_xyzful0(i+1,1:3)-real_xyzful0(i,1:3)
+                        dxyz(1:3) = updaterealxyzful(i+1,1:3)-updaterealxyzful(i,1:3)
+                    elseif ( i .eq. real_npts) then
+                        dxyz0(1:3)= real_xyzful0(i,1:3)-real_xyzful0(i-1,1:3)
+                        dxyz(1:3) = updaterealxyzful(i,1:3)-updaterealxyzful(i-1,1:3)
+                    else
+                        dxyz0(1:3)= real_xyzful0(i+1,1:3)-real_xyzful0(i-1,1:3)
+                        dxyz(1:3) = updaterealxyzful(i+1,1:3)-updaterealxyzful(i-1,1:3)
+                    endif
+                    dl0       = dsqrt(dxyz0(1)**2+dxyz0(2)**2+dxyz0(3)**2)
+                    dl        = dsqrt(dxyz(1)**2+dxyz(2)**2+dxyz(3)**2)
+                    xlmn0(1:3)= dxyz0(1:3)/dl0
+                    xlmn(1:3) = dxyz(1:3)/dl
+                    call Beam%RotateMatrix(i,xlmn0(1),xlmn0(2),xlmn0(3),xlmn(1),xlmn(2),xlmn(3))
+                    temp(1:3) = matmul(Beam%rotMat(i,:,:), (/Beam%fake_xyz0(j,1), 0.0d0, Beam%fake_xyz0(j,3)/))
+                    ! Default The fake point on the plane (Beam%fake_xyz0(j,1:3) = (x,y,z)) is in the same plane as the point on the centre axis(real_xyzful0(i,1:3) = (0,y,0)).
+                    tempdxyz(1:3) = updaterealxyzful(i,1:3) - real_xyzful0(i,1:3)
+                    Beam%fake_xyz(j,1:3) = temp(1:3)+tempdxyz(1:3)+real_xyzful0(i,1:3)
+                endif
+            enddo
+        enddo
     end subroutine Beam_Updatexyz
 
     subroutine Beam_Updatevel(updaterealvelful)
@@ -143,10 +167,85 @@ module SolidBody
         implicit none
         class(Body), intent(inout) :: this
         integer:: i
-        
+        ! real(8):: l0,m0,n0,l,m,n,angle,pi
+        ! real(8):: v_0(3),e1(3),e2(3),e3(3),r1(3,3)
+        ! pi=4.0*datan(1.0d0)
+        ! v_0= (/l0,m0,n0/)
+        ! e1 = (/l,m,n/)
+        ! e2 = 0.0d0
+        ! e3 = 0.0d0
+        ! call angle_between_vectors(v_0,e1)
+        ! if (angle .gt. 1e-3) then
+        !     call normal_vector(v_0,e1,e2)
+        !     call normal_vector(e1,e2,e3)
+        ! else
+        !     e1 = (/1.0d0,0.0d0,0.0d0/)
+        !     e2 = (/0.0d0,1.0d0,0.0d0/)
+        !     e3 = (/0.0d0,0.0d0,1.0d0/)
+        ! endif
+        ! r1(1,1)  =   e1(1)
+        ! r1(1,2)  =   e1(2)
+        ! r1(1,3)  =   e1(3)
+        ! r1(2,1)  =   e2(1)
+        ! r1(2,2)  =   e2(2)
+        ! r1(2,3)  =   e2(3)
+        ! r1(3,1)  =   e3(1)
+        ! r1(3,2)  =   e3(2)
+        ! r1(3,3)  =   e3(3)
+        ! this%rotMat(i,:,:) = r1
+        ! contains
         real(8):: l0,m0,n0,l,m,n,angle,pi
         real(8):: v_1(3),v_2(3),lamda(0:3),nn(3),r1(3,3)
-        
+        pi=4.0*datan(1.0d0)
+        v_1=(/l0,m0,n0/)
+        v_2=(/l,m,n/)
+        nn=0.0d0
+        lamda=0.0d0
+        angle=0.0d0
+        call angle_between_vectors(v_1,v_2)
+        if (angle .gt. 1e-3) then
+            call normal_vector(v_1,v_2,nn)
+            lamda(0)=cos(angle*0.5)
+            lamda(1)=nn(1)*sin(angle*0.5)
+            lamda(2)=nn(2)*sin(angle*0.5)
+            lamda(3)=nn(3)*sin(angle*0.5)
+            r1(1,1) = 2*(lamda(0)**2+lamda(1)**2)-1
+            r1(1,2) = 2*(lamda(1)*lamda(2)-lamda(0)*lamda(3))
+            r1(1,3) = 2*(lamda(1)*lamda(3)+lamda(0)*lamda(2))
+            r1(2,1) = 2*(lamda(2)*lamda(1)+lamda(0)*lamda(3))
+            r1(2,2) = 2*(lamda(0)**2+lamda(2)**2)-1
+            r1(2,3) = 2*(lamda(2)*lamda(3)-lamda(0)*lamda(1))
+            r1(3,1) = 2*(lamda(3)*lamda(1)-lamda(0)*lamda(2))
+            r1(3,2) = 2*(lamda(3)*lamda(2)+lamda(0)*lamda(1))
+            r1(3,3) = 2*(lamda(0)**2+lamda(3)**2)-1
+        else
+            r1(1,:) = (/1.0d0,0.0d0,0.0d0/)
+            r1(2,:) = (/0.0d0,1.0d0,0.0d0/)
+            r1(3,:) = (/0.0d0,0.0d0,1.0d0/)
+        endif
+        this%rotMat(i,:,:) = r1
+        contains
+        subroutine normal_vector(A,B,C)
+            real(8):: A(3),B(3),C(3)
+            real(8) :: norm_C
+            C(1)=A(2)*B(3) - A(3)*B(2)
+            C(2)=A(3)*B(1) - A(1)*B(3)
+            C(3)=A(1)*B(2) - A(2)*B(1)
+            norm_C = sqrt(C(1)**2 + C(2)**2 + C(3)**2)
+            if (abs(norm_C - 0.0d0).gt.1e-10) then
+                C = C / norm_C
+            endif
+            return
+        end subroutine normal_vector
+        subroutine angle_between_vectors(A,B)
+            real(8), intent(in) :: A(3),B(3)
+            real(8) :: dot_product, magnitude1, magnitude2
+            dot_product = A(1)*B(1)+A(2)*B(2)+A(3)*B(3)
+            magnitude1 = sqrt(A(1)**2+A(2)**2+A(3)**2)
+            magnitude2 = sqrt(B(1)**2+B(2)**2+B(3)**2)
+            angle = acos(dot_product / (magnitude1 * magnitude2))
+            return
+        end subroutine angle_between_vectors
     end subroutine Section_RotateMatrix
 
     subroutine Unstructured_Fake_Nodes(this)
