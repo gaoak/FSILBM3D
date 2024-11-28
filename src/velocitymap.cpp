2b3d405aadd422587aed7eb0cdc7059499708a41
astrobearrrrr <895208821@qq.com>
2024-11-28 22:04:06 +0800

    fix getuuu_ and setuuu_

--- src/velocitymap.cpp
+++ src/velocitymap.cpp
@@ -4,22 +4,22 @@
 #include<iostream>
 extern "C" {
     void initumap(int32_t &np);
-    void thread_setumap(int32_t &p, int64_t &index, double &value);
+    void thread_setumap(int32_t &p, int64_t &index, std::vector<double> &value);
     void mergeumap();
-    void setumap(int64_t &index, double &value);
-    void getumap(int64_t &index, double &value);
+    void setumap(int64_t &index, std::vector<double> &value);
+    void getumap(int64_t &index, std::vector<double> &value);
     void printumap();
 }
 
-std::vector<std::map<int64_t, double>> threadmap;
-std::map<int64_t, double> umap;
+std::vector<std::map<int64_t, std::vector<double>>> threadmap;
+std::map<int64_t, std::vector<double>> umap;
 
 void initumap(int32_t &np) {
     threadmap.resize(np);
     umap.clear();
 }
 
-void thread_setumap(int32_t &p, int64_t &index, double &value) {
+void thread_setumap(int32_t &p, int64_t &index, std::vector<double> &value) {
     threadmap[p][index] = value;
 }
 
@@ -33,11 +33,11 @@ void mergeumap() {
     threadmap.clear();
 }
 
-void setumap(int64_t &index, double &value) {
+void setumap(int64_t &index, std::vector<double> &value) {
     umap[index] = value;
 }
 
-void getumap(int64_t &index, double &value) {
+void getumap(int64_t &index, std::vector<double> &value) {
     value = umap[index];
 }
 
@@ -45,7 +45,14 @@ void getumap(int64_t &index, double &value) {
 void printumap() {
     std::cout << "umap with size " << umap.size() << std::endl;
     for(const auto &it:umap) {
-        std::cout << it.first << ", " << it.second << std::endl;
+        std::cout << it.first << ", [";
+        for (size_t i = 0; i < it.second.size(); ++i) {
+            std::cout << it.second[i];
+            if (i < it.second.size() - 1) {
+                std::cout << ", ";  // 添加逗号分隔符
+            }
+        }
+        std::cout << "]" << std::endl;
     }
     std::cout << "================" << std::endl;
 }
