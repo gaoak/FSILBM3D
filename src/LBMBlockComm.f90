86f6b4824ea80405209d7a433a22a748ef8a4364
Ankang Gao <ankanggao@ustc.edu.cn>
2024-12-15 01:39:43 +0800

    more fix

--- src/LBMBlockComm.f90
+++ src/LBMBlockComm.f90
@@ -7,8 +7,9 @@ module LBMBlockComm
     type :: CommPair
         integer:: fatherId
         integer:: sonId
-        integer:: sondirs(1:6) ! 0 no need communication; 1 need communication
-        integer:: type ! local (0) or mpi (1)
+        integer:: sds(1:6) ! 0 no need communication; 1(min),-1(max) need communication
+        integer:: s(1:6),f(1:6),si(1:6),fi(1:6) ! s son's boundary layer; si son's first inner layer
+        integer:: islocal ! local (0) or mpi (1)
     end type CommPair
     integer:: m_npairs
     type(CommPair),allocatable:: commpairs(:)
@@ -20,7 +21,7 @@ module LBMBlockComm
         character(LEN=40),intent(in):: filename
         character(LEN=40):: keywordstr
         character(LEN=256):: buffer
-        integer:: i,j
+        integer:: i,j,sxD,syD,szD,ratio,fId,sId,islocal
         ! allocate and read commpairs from file
         open(unit=111, file=filename, status='old', action='read')
         keywordstr = 'Communication'
@@ -30,14 +31,40 @@ module LBMBlockComm
         allocate(commpairs(m_npairs))
         do i=1,m_npairs
             call readNextData(111, buffer)
-            read(buffer,*) commpairs(i)%fatherId,commpairs(i)%sonId,commpairs(i)%type
+            read(buffer,*) fId,sId,islocal
+            commpairs(i)%fatherId = fId
+            commpairs(i)%sonId = sId
+            commpairs(i)%islocal = islocal
             do j=1,6
-                if(LBMblks(LBMblksIndex(commpairs(i)%sonId))%BndConds(j).eq.BCfluid) then
-                    commpairs(i)%sondirs(j) = 1
+                if(LBMblks(LBMblksIndex(sId))%BndConds(j).eq.BCfluid) then
+                    if(mod(j,2).eq.1) then
+                        commpairs(i)%sds(j) = 1
+                    else
+                        commpairs(i)%sds(j) = -1
+                    endif
                 else
-                    commpairs(i)%sondirs(j) = 0
+                    commpairs(i)%sds(j) = 0
                 endif
             enddo
+            sxD = LBMblks(sId)%xDim
+            syD = LBMblks(sId)%yDim
+            szD = LBMblks(sId)%zDim
+            commpairs(i)%s([1,3,5]) = 1
+            commpairs(i)%s(2) = sxD
+            commpairs(i)%s(4) = syD
+            commpairs(i)%s(6) = szD
+            commpairs(i)%f(1) = floor((LBMblks(sId)%xmin - LBMblks(fId)%xmin) / LBMblks(fId)%dh + 1.5d0)
+            commpairs(i)%f(3) = floor((LBMblks(sId)%ymin - LBMblks(fId)%ymin) / LBMblks(fId)%dh + 1.5d0)
+            commpairs(i)%f(5) = floor((LBMblks(sId)%zmin - LBMblks(fId)%zmin) / LBMblks(fId)%dh + 1.5d0)
+            ratio = floor(LBMblks(fId)%dh / LBMblks(sId)%dh + 0.5d0)
+            sxD = (sxD - 1) / ratio
+            syD = (syD - 1) / ratio
+            szD = (szD - 1) / ratio
+            commpairs(i)%f(2) = commpairs(i)%f(1) + sxD
+            commpairs(i)%f(4) = commpairs(i)%f(3) + syD
+            commpairs(i)%f(6) = commpairs(i)%f(5) + szD
+            commpairs(i)%si(1:6) = commpairs(i)%s(1:6) + commpairs(i)%sds(1:6)
+            commpairs(i)%fi(1:6) = commpairs(i)%f(1:6) + commpairs(i)%sds(1:6)
         enddo
         close(111)
     end subroutine Read_Comm_Pair
@@ -55,49 +82,49 @@ module LBMBlockComm
         use FluidDomain
         implicit none
         type(CommPair),intent(in):: pair
-        integer:: fxS,fyS,fzS,fxE,fyE,fzE,sxD,syD,szD,f(6)
-        sxD = LBMblks(pair%sonId)%xDim
-        syD = LBMblks(pair%sonId)%yDim
-        szD = LBMblks(pair%sonId)%zDim
-        fxS = floor((LBMblks(pair%sonId)%xmin - LBMblks(pair%fatherId)%xmin) / LBMblks(pair%fatherId)%dh + 1.5d0)
-        fyS = floor((LBMblks(pair%sonId)%ymin - LBMblks(pair%fatherId)%ymin) / LBMblks(pair%fatherId)%dh + 1.5d0)
-        fzS = floor((LBMblks(pair%sonId)%zmin - LBMblks(pair%fatherId)%zmin) / LBMblks(pair%fatherId)%dh + 1.5d0)
-        fxE = fxS + sxD - 1
-        fyE = fyS + syD - 1
-        fzE = fzS + szD - 1
-        f = pair%sondirs
+        integer:: s(1:6),f(1:6),si(1:6),fi(1:6)
         ! x direction
-        if(f(1).eq.1) then
-            LBMblks(pair%sonId)%fIn(1:szD,1:syD,1,0:lbmDim) = LBMblks(pair%fatherId)%fIn(fzS:fzE,fyS:fyE,fxS,0:lbmDim)
-            LBMblks(pair%fatherId)%fIn((fzS+f(6)):(fzE-f(5)),(fyS+f(4)):(fyE-f(3)),(fxS+1),0:lbmDim) =&
-                LBMblks(pair%sonId)%fIn((1+f(6)):(szD-f(5)),(1+f(4)):(syD-f(3)),2,0:lbmDim)
+        s = pair%s
+        f = pair%f
+        si = pair%si
+        fi = pair%fi
+        if(pair%sds(1).eq.1) then
+            LBMblks(pair%sonId)%fIn(s(5):s(6),s(3):s(4),s(1),0:lbmDim) =&
+                LBMblks(pair%fatherId)%fIn(f(5):f(6),f(3):f(4),f(1),0:lbmDim)
+            LBMblks(pair%fatherId)%fIn(fi(5):fi(6),fi(3):fi(4),fi(1),0:lbmDim) =&
+                LBMblks(pair%sonId)%fIn(si(5):si(6),si(3):si(4),si(1),0:lbmDim)
         endif
-        if(f(2).eq.1) then
-            LBMblks(pair%sonId)%fIn(1:szD,1:syD,sxD,0:lbmDim) = LBMblks(pair%fatherId)%fIn(fzS:fzE,fyS:fyE,fxE,0:lbmDim)
-            LBMblks(pair%fatherId)%fIn((fzS+f(6)):(fzE-f(5)),(fyS+f(4)):(fyE-f(3)),(fxE-1),0:lbmDim) =&
-                LBMblks(pair%sonId)%fIn((1+f(6)):(szD-f(5)),(1+f(4)):(syD-f(3)),sxD-1,0:lbmDim)
+        if(pair%sds(2).eq.-1) then
+            LBMblks(pair%sonId)%fIn(s(5):s(6),s(3):s(4),s(2),0:lbmDim) =&
+                LBMblks(pair%fatherId)%fIn(f(5):f(6),f(3):f(4),f(2),0:lbmDim)
+            LBMblks(pair%fatherId)%fIn(fi(5):fi(6),fi(3):fi(4),fi(2),0:lbmDim) =&
+                LBMblks(pair%sonId)%fIn(si(5):si(6),si(3):si(4),si(2),0:lbmDim)
         endif
         ! y direction
-        if(f(3).eq.1) then
-            LBMblks(pair%sonId)%fIn(1:szD,1,1:sxD,0:lbmDim) = LBMblks(pair%fatherId)%fIn(fzS:fzE,fyS,fxS:fxE,0:lbmDim)
-            LBMblks(pair%fatherId)%fIn((fzS+f(6)):(fzE-f(5)),(fyS+1),(fxS+f(2)):(fxE-f(1)),0:lbmDim) =&
-                LBMblks(pair%sonId)%fIn((1+f(6)):(szD-f(5)),2,(1+f(2)):(sxD-f(1)),0:lbmDim)
+        if(pair%sds(3).eq.1) then
+            LBMblks(pair%sonId)%fIn(s(5):s(6),s(3),s(1):s(2),0:lbmDim) =&
+                LBMblks(pair%fatherId)%fIn(f(5):f(6),f(3),f(1):f(2),0:lbmDim)
+            LBMblks(pair%fatherId)%fIn(fi(5):fi(6),fi(3),fi(1):fi(2),0:lbmDim) =&
+                LBMblks(pair%sonId)%fIn(si(5):si(6),si(3),si(1):si(2),0:lbmDim)
         endif
-        if(f(4).eq.1) then
-            LBMblks(pair%sonId)%fIn(1:szD,syD,1:sxD,0:lbmDim) = LBMblks(pair%fatherId)%fIn(fzS:fzE,fyE,fxS:fxE,0:lbmDim)
-            LBMblks(pair%fatherId)%fIn((fzS+f(6)):(fzE-f(5)),(fyE-1),(fxS+f(2)):(fxE-f(1)),0:lbmDim) =&
-                LBMblks(pair%sonId)%fIn((1+f(6)):(szD-f(5)),(syD-1),(1+f(2)):(sxD-f(1)),0:lbmDim)
+        if(pair%sds(4).eq.-1) then
+            LBMblks(pair%sonId)%fIn(s(5):s(6),s(4),s(1):s(2),0:lbmDim) =&
+                LBMblks(pair%fatherId)%fIn(f(5):f(6),f(4),f(1):f(2),0:lbmDim)
+            LBMblks(pair%fatherId)%fIn(fi(5):fi(6),fi(4),fi(1):fi(2),0:lbmDim) =&
+                LBMblks(pair%sonId)%fIn(si(5):si(6),si(4),si(1):si(2),0:lbmDim)
         endif
         ! z direction
-        if(f(5).eq.1) then
-            LBMblks(pair%sonId)%fIn(1,1:syD,1:sxD,0:lbmDim) = LBMblks(pair%fatherId)%fIn(fzS,fyS:fyE,fxS:fxE,0:lbmDim)
-            LBMblks(pair%fatherId)%fIn((fzS+1),(fyS+f(4)):(fyE-f(3)),(fxS+f(2)):(fxE-f(1)),0:lbmDim) =&
-                LBMblks(pair%sonId)%fIn(2,(1+f(4)):(syD-f(3)),(1+f(2)):(sxD-f(1)),0:lbmDim)
+        if(pair%sds(5).eq.1) then
+            LBMblks(pair%sonId)%fIn(s(5),s(3):s(4),s(1):s(2),0:lbmDim) =&
+                LBMblks(pair%fatherId)%fIn(f(5),f(3):f(4),f(1):f(2),0:lbmDim)
+            LBMblks(pair%fatherId)%fIn(fi(5),fi(3):fi(4),fi(1):fi(2),0:lbmDim) =&
+                LBMblks(pair%sonId)%fIn(si(5),si(3):si(4),si(1):si(2),0:lbmDim)
         endif
-        if(f(6).eq.1) then
-            LBMblks(pair%sonId)%fIn(szD,1:syD,1:sxD,0:lbmDim) = LBMblks(pair%fatherId)%fIn(fzE,fyS:fyE,fxS:fxE,0:lbmDim)
-            LBMblks(pair%fatherId)%fIn((fzE-1),(fyS+f(4)):(fyE-f(3)),(fxS+f(2)):(fxE-f(1)),0:lbmDim) =&
-                LBMblks(pair%sonId)%fIn((szD-1),(1+f(4)):(syD-f(3)),(1+f(2)):(sxD-f(1)),0:lbmDim)
+        if(pair%sds(6).eq.-1) then
+            LBMblks(pair%sonId)%fIn(s(6),s(3):s(4),s(1):s(2),0:lbmDim) =&
+                LBMblks(pair%fatherId)%fIn(f(6),f(3):f(4),f(1):f(2),0:lbmDim)
+            LBMblks(pair%fatherId)%fIn(fi(6),fi(3):fi(4),fi(1):fi(2),0:lbmDim) =&
+                LBMblks(pair%sonId)%fIn(si(6),si(3):si(4),si(1):si(2),0:lbmDim)
         endif
     end subroutine ExchangeDataSerial
 end module LBMBlockComm
