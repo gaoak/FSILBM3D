3ff089ae46b8d0d1d4c978f6f62792ef43fe87cf
Small Luo <xianguangLuo@mail.ustc.edu.cn>
2025-03-02 09:32:19 +0800

    update examples

--- postprocess/matlabWriteVtks/reWirteSolidVTK.m
+++ postprocess/matlabWriteVtks/reWirteSolidVTK.m
@@ -8,9 +8,9 @@ for n = 1:nfile
     [readName2, writeName2] = generateSolidPath(readPath,writePath,time,2);
     [readName1, writeName1] = generateSolidPath(readPath,writePath,time,1);
     % Read mesh data
-    % solid2 = readAscallSolid(readName2,exVel,time);
+    solid2 = readAscallSolid(readName2,exVel,time);
     solid1 = readAscallSolid(readName1,exVel,time);
     % Write mesh data in vtks
-    % writeSolidVTK(solid2,writeName2)
+    writeSolidVTK(solid2,writeName2)
     writeSolidVTK(solid1,writeName1)
 end
