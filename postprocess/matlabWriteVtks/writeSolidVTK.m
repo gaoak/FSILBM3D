e6f3f15836705e99bfe02a110f2d10b8915789ad
Small Luo <xianguangLuo@mail.ustc.edu.cn>
2025-03-04 11:04:42 +0800

    fix solid file read and write

--- postprocess/matlabWriteVtks/writeSolidVTK.m
+++ postprocess/matlabWriteVtks/writeSolidVTK.m
@@ -16,10 +16,10 @@ fprintf(fid, 'POINTS %d float\n', solid.nx * solid.ny * solid.nz);
 
 % Write coordinates 
 for i = 1:solid.nx
-    fprintf(fid, '%f %f %f\n', solid.x(i), solid.y(i), solid.z(1));
+    fprintf(fid, '%f %f %f\n', solid.x(i), solid.y(i), solid.zl(i));
 end
 for k = 1:solid.nx
-    fprintf(fid, '%f %f %f\n', solid.x(k), solid.y(k), solid.z(2));
+    fprintf(fid, '%f %f %f\n', solid.x(k), solid.y(k), solid.zr(k));
 end
 % Write point data
 fprintf(fid, 'POINT_DATA %d\n', solid.nx * solid.ny * solid.nz);
