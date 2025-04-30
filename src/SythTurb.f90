b0e8c221c433351a43f39b68bf74ffa2cfc408c6
astrobearrrrr <895208821@qq.com>
2025-04-30 20:32:33 +0800

    optimise input

--- src/SythTurb.f90
+++ src/SythTurb.f90
@@ -1,25 +1,40 @@
 module SythTurb
+    use ConstParams
     use FlowCondition
     implicit none
     private
     public:: SythTurb_Input
     integer,parameter:: Dim=3,Nk=200
     real(8),parameter:: RL=40000
-    real(8),parameter:: L=1
-    real(8):: nu,eta,epsilon,u_eta,k0,kc,K(1:Nk+1),E_k(1:Nk+1)
+    ! real(8),parameter:: L=1
+    ! real(8),parameter:: nu=1e-6
+    real(8):: eta,epsilon,u_eta,k0,kc,K(1:Nk+1),E_k(1:Nk+1)
+    real(8):: ukn(1:Nk),kn(1:Nk)
+    real(8):: tke,tn,tm
+    real(8):: m_nu,m_L
+    integer:: NX,NY,NZ,Nt
+    real(8),allocatable:: x(:),y(:),z(:),t(:)
+    real(8),allocatable:: uHIT(:,:,:,:,:)
 
     contains
 
-    subroutine SythTurb_Input()
+    subroutine SythTurb_Input(xDim,yDim,zDim)
         implicit none
-        integer:: i
+        integer:: xDim,yDim,zDim
+        integer:: i,j,m
         real(8):: d
-        nu      = 1d-6
-        eta     = L/(RL)**(3d0/4d0)
-        epsilon = (nu**3d0)/(eta**4d0)
+        real(8),allocatable:: KN_(:,:,:),Psi(:,:),Sigma(:,:,:),point_uHIT(:,:)
+        NX      = xDim
+        NY      = yDim
+        NZ      = zDim
+        Nt      = 1
+        m_L     = flow%Lref
+        m_nu    = flow%nu
+        eta     = m_L/(RL)**(3d0/4d0)
+        epsilon = (m_nu**3d0)/(eta**4d0)
 
-        u_eta   = (epsilon*nu)**(1d0/4d0)
-        k0      = 0.2d0/L
+        u_eta   = (epsilon*m_nu)**(1d0/4d0)
+        k0      = 0.2d0/m_L
         kc      = 2d0/eta
 
         do i = 1,Nk+1
@@ -27,8 +42,47 @@ module SythTurb
             K(i) = exp(d)
         enddo
         call Ek()
-        if (Dim.eq.2) call Flow_Generation_dim2()
-        if (Dim.eq.3) call Flow_Generation_dim3()
+
+        allocate(x(NX),y(NY),z(NZ),t(Nt))
+        do i = 1,NX
+            x(i) = (m_L-0)*dble(i-1)/dble(NX-1)+0d0
+        enddo
+        do j = 1,NY
+            y(j) = (m_L-0)*dble(j-1)/dble(NY-1)+0d0
+        enddo
+        do m = 1,NZ
+            z(m) = (m_L-0)*dble(m-1)/dble(NZ-1)+0d0
+        enddo
+        if (Nt.eq.1)then
+            t(1) = tm
+        else
+            do i = 1,Nt
+                t(i) = (tm-0)*dble(i-1)/dble(Nt-1)+0d0
+            enddo
+        endif
+
+        ukn = sqrt((E_k(1:Nk)+E_k(2:Nk+1))/2d0*(K(2:Nk+1)-K(1:Nk)))
+        kn  = 0.5d0*(K(1:Nk)+K(2:Nk+1))
+
+        tke = sum((E_k(1:Nk)+E_k(2:Nk+1))/2d0*(K(2:Nk+1)-K(1:Nk)))
+        tn  = (m_nu/epsilon)**0.5d0
+        tm  = 100d0*tn
+
+        allocate(uHIT(Nt,NX,NY,NZ,Dim))
+        uHIT = 0.0d0
+
+        allocate(KN_(Nt,Nk,Dim),Psi(Nt,Nk),Sigma(Nt,Nk,Dim),point_uHIT(Nt,Dim))
+
+        if (Dim.eq.3) call Flow_Generation(KN_,Sigma,Psi)
+
+        do i = 1,NX
+            do j = 1,NY
+                do m = 1,NZ
+                    call point_uHIT_Generate(KN_,Sigma,Psi,i,j,m,point_uHIT)
+                    uHIT(:,i,j,m,:) = point_uHIT(:,:)
+                enddo
+            enddo
+        enddo
     endsubroutine
 
     subroutine Ek()
@@ -41,146 +95,28 @@ module SythTurb
 
         c    = 1.5d0
         beta = 5.2d0
-        p0   = 2d0
+        p0   = 2.0d0
         cl   = 6.78d0
         cn   = 0.4d0
 
         do i = 1,Nk+1
-            fl(i)  = (K(i)*L/sqrt((K(i)*L)**2d0+cl))**(5d0/3d0+p0)
+            fl(i)  = (K(i)*m_L/sqrt((K(i)*m_L)**2d0+cl))**(5d0/3d0+p0)
             fn(i)  = exp(-beta*(((K(i)*eta)**4d0+cn**4d0)**0.25d0-cn))
             E_k(i) = c*epsilon**(2d0/3d0)*K(i)**(-5d0/3d0)*fl(i)*fn(i)
         enddo
     endsubroutine
 
-    subroutine Flow_Generation_dim2()
+    subroutine Flow_Generation(KN_,Sigma,Psi)
         implicit none
         ! Turbulence power spectrum E(K) with -5/3 Kolmogorov spectrum 
         ! von Karman spectrum
-        real(8), parameter:: pi = 3.141592653589793d0
-        real(8):: ukn(1:Nk),kn(1:Nk)
-        real(8):: tke,tn,tm
-        integer:: NX,NY,NZ,Nt
-        real(8),allocatable:: x(:),y(:),z(:),t(:)
-        real(8),allocatable:: Psi(:,:),Phi(:,:),Sigma(:,:,:),k_dir(:,:,:)
-        real(8),allocatable:: KN_(:,:,:),uHIT(:,:,:,:),tmp(:,:),ukn_(:,:),tmp_(:,:,:),uHIT_(:,:,:)
-        integer:: i,j,m
-        real(8),allocatable:: r(:,:)
-
-
-        ukn = sqrt((E_k(1:Nk)+E_k(2:Nk+1))/2d0*(K(2:Nk+1)-K(1:Nk)))
-        kn  = 0.5d0*(K(1:Nk)+K(2:Nk+1))
-
-        tke = sum((E_k(1:Nk)+E_k(2:Nk+1))/2d0*(K(2:Nk+1)-K(1:Nk)))
-        tn  = (nu/epsilon)**0.5d0
-        tm  = 100d0*tn
-        
-        NX = 150
-        NY = 150
-        NZ = 150
-        Nt = 1
-        allocate(x(NX),y(NY),z(NZ),t(Nt))
-        do i = 1,NX
-            x(i) = (L-0)*dble(i-1)/dble(NX-1)+0d0
-        enddo
-        do j = 1,NY
-            y(j) = (L-0)*dble(j-1)/dble(NY-1)+0d0
-        enddo
-        do m = 1,NZ
-            z(m) = (L-0)*dble(m-1)/dble(NZ-1)+0d0
-        enddo
-        if (Nt.eq.1)then
-            t(1) = tm
-        else
-            do i = 1,Nt
-                t(i) = (tm-0)*dble(i-1)/dble(Nt-1)+0d0
-            enddo
-        endif
-
-        ! Dim=2
-        allocate(r(Nt,Nk),Psi(Nt,Nk),Phi(Nt,Nk),Sigma(Nt,Nk,Dim),k_dir(Nt,Nk,Dim))
-        call random_number(r)
-        Psi(:,:) = 2d0*pi*r(:,:)
-        call random_number(r)
-        Phi(:,:) = 2d0*pi*r(:,:)
-        Sigma=0d0;
-        k_dir=0d0;
-        k_dir(:,:,1)=cos(Phi(:,:));
-        k_dir(:,:,2)=sin(Phi(:,:));
-
-        allocate(KN_(Nt,Nk,Dim),uHIT(Nt,NX,NY,Dim),tmp(Nt,Nk),ukn_(Nt,Nk),tmp_(Nt,Nk,Dim),uHIT_(Nt,Nk,Dim))
-        uHIT = 0d0
-        call repmat1dim3(kn,Nk,Nt,1,Dim,KN_)
-        KN_ = KN_(:,:,:)*k_dir(:,:,:)
-    
-        Sigma(:,:,1)=-sin(Phi(:,:));
-        Sigma(:,:,2)= cos(Phi(:,:));
-
-        do i=1,NX
-            do j=1,NY
-                tmp = cos(KN_(:,:,1)*x(i)+KN_(:,:,2)*y(j)+Psi(:,:))
-
-                call repmat1dim2(ukn,Nk,Nt,1,ukn_)
-
-                call repmat2dim3(tmp(:,:)*ukn_(:,:),Nt,Nk,1,1,Dim,tmp_)
-
-                uHIT_(:,:,:) = tmp_(:,:,:)*Sigma(:,:,:)
-                uHIT(:,i,j,:) = sum(uHIT_,dim=2)
-            enddo
-        enddo
-
-        ! open(111,file='1.dat',position='append')
-        ! write(111,'(150E20.10)') uHIT(1,:,:,1)
-        ! close(111)
-
-    endsubroutine
-
-    subroutine Flow_Generation_dim3()
-        implicit none
-        ! Turbulence power spectrum E(K) with -5/3 Kolmogorov spectrum 
-        ! von Karman spectrum
-        real(8), parameter:: pi = 3.141592653589793d0
-        real(8):: ukn(1:Nk),kn(1:Nk)
-        real(8):: tke,tn,tm
-        integer:: NX,NY,NZ,Nt
-        real(8),allocatable:: x(:),y(:),z(:),t(:)
-        real(8),allocatable:: Theta(:,:),Psi(:,:),Phi(:,:),Alpha(:,:),Sigma(:,:,:),k_dir(:,:,:)
+        real(8):: r(Nt,Nk),Theta(Nt,Nk),Phi(Nt,Nk),Alpha(Nt,Nk),k_dir(Nt,Nk,Dim)
+        real(8),intent(out):: Psi(Nt,Nk),Sigma(Nt,Nk,Dim)
         real(8):: theta_,psi_,phi_,alpha_,Ry(3,3),Rz(3,3),sigma0(3),sigma_(3)
-        real(8),allocatable:: KN_(:,:,:),uHIT(:,:,:,:,:),tmp(:,:),ukn_(:,:),tmp_(:,:,:),uHIT_(:,:,:)
+        real(8),intent(out):: KN_(Nt,Nk,Dim)
         integer:: i,j,m
-        real(8),allocatable:: r(:,:)
-
-
-        ukn = sqrt((E_k(1:Nk)+E_k(2:Nk+1))/2d0*(K(2:Nk+1)-K(1:Nk)))
-        kn  = 0.5d0*(K(1:Nk)+K(2:Nk+1))
-
-        tke = sum((E_k(1:Nk)+E_k(2:Nk+1))/2d0*(K(2:Nk+1)-K(1:Nk)))
-        tn  = (nu/epsilon)**0.5d0
-        tm  = 100d0*tn
-        
-        NX = 150
-        NY = 150
-        NZ = 3
-        Nt = 1
-        allocate(x(NX),y(NY),z(NZ),t(Nt))
-        do i = 1,NX
-            x(i) = (L-0)*dble(i-1)/dble(NX-1)+0d0
-        enddo
-        do j = 1,NY
-            y(j) = (L-0)*dble(j-1)/dble(NY-1)+0d0
-        enddo
-        do m = 1,NZ
-            z(m) = (L-0)*dble(m-1)/dble(NZ-1)+0d0
-        enddo
-        if (Nt.eq.1)then
-            t(1) = tm
-        else
-            do i = 1,Nt
-                t(i) = (tm-0)*dble(i-1)/dble(Nt-1)+0d0
-            enddo
-        endif
 
         ! Dim=3
-        allocate(r(Nt,Nk),Theta(Nt,Nk),Psi(Nt,Nk),Phi(Nt,Nk),Alpha(Nt,Nk),Sigma(Nt,Nk,Dim),k_dir(Nt,Nk,Dim))
         call random_number(r)
         Theta(:,:) = asin(r(:,:))
         call random_number(r)
@@ -227,32 +163,32 @@ module SythTurb
         k_dir(:,:,2)=sin(Phi(:,:))*sin(Theta(:,:));
         k_dir(:,:,3)=cos(Theta(:,:));
 
-        allocate(KN_(Nt,Nk,Dim),uHIT(Nt,NX,NY,NZ,Dim),tmp(Nt,Nk),ukn_(Nt,Nk),tmp_(Nt,Nk,Dim),uHIT_(Nt,Nk,Dim))
-        uHIT = 0d0
         call repmat1dim3(kn,Nk,Nt,1,Dim,KN_)
         KN_ = KN_(:,:,:)*k_dir(:,:,:)
 
         Sigma(:,:,1)=-sin(Phi(:,:));
         Sigma(:,:,2)= cos(Phi(:,:));
 
-        do i=1,NX
-            do j=1,NY
-                do m=1,NZ
-                    tmp = cos(KN_(:,:,1)*x(i)+KN_(:,:,2)*y(j)+KN_(:,:,3)*z(m)+Psi(:,:))
-
-                    call repmat1dim2(ukn,Nk,Nt,1,ukn_)
-
-                    call repmat2dim3(tmp(:,:)*ukn_(:,:),Nt,Nk,1,1,Dim,tmp_)
-
-                    uHIT_(:,:,:) = tmp_(:,:,:)*Sigma(:,:,:)
-                    uHIT(:,i,j,m,:) = sum(uHIT_,dim=2)
-                enddo
-            enddo
-        enddo
-
         ! open(111,file='1.dat',position='append')
         ! write(111,'(150E20.10)') uHIT(1,:,:,1,1)
         ! close(111)
+    endsubroutine
+
+    subroutine point_uHIT_Generate(KN_,Sigma,Psi,i,j,m,point_uHIT)
+        implicit none
+        real(8),intent(in):: KN_(Nt,Nk,Dim),Sigma(Nt,Nk,Dim),Psi(Nt,Nk)
+        real(8):: tmp(Nt,Nk),ukn_(Nt,Nk),tmp_(Nt,Nk,Dim),uHIT_(Nt,Nk,Dim)
+        real(8),intent(out):: point_uHIT(Nt,Dim)
+        integer:: i,j,m
+
+        tmp = cos(KN_(:,:,1)*x(i)+KN_(:,:,2)*y(j)+KN_(:,:,3)*z(m)+Psi(:,:))
+
+        call repmat1dim2(ukn,Nk,Nt,1,ukn_)
+
+        call repmat2dim3(tmp(:,:)*ukn_(:,:),Nt,Nk,1,1,Dim,tmp_)
+
+        uHIT_(:,:,:) = tmp_(:,:,:)*Sigma(:,:,:)
+        point_uHIT(:,:) = sum(uHIT_,dim=2)
     endsubroutine
 
     subroutine repmat1dim3(mat,matDim2,Dim1,Dim2,Dim3,mat_)
